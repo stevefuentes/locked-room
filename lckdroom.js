b7de35b048be909af049a0a7889003db5eb0ee6f
Steve <steve@Steves-Mac-Pro.local>
2013-11-21 02:28:22 -0500

    Started to hange structure to OOP

--- lckdroom.js
+++ lckdroom.js
@@ -77,6 +77,7 @@ function listInv(){
   }
     print(string);
 }
+
 function requestInput(){
   
   if ($('.inputWrap').length){
@@ -108,7 +109,7 @@ function requestInput(){
         words = $inputArr[last].split(" ");
         console.log($inputArr);
         analyzeInput(words[0]);
-        if (debug){
+        if (locked_room.debug){
           print('<div>'+$inputArr[last]+'</div>'); 
 
         }
@@ -215,7 +216,7 @@ function analyzeInput(switchvar){
 
     case 'N': case 'NORTH':
       if (player.location.n != undefined){
-        player.location = locations[player.location.n];
+        player.setLocation(locations[player.location.n]);
         print('You went North!');
         print('</br>'+player.location.description);
       }else{
@@ -224,7 +225,7 @@ function analyzeInput(switchvar){
       break;
     case 'S': case 'SOUTH':
       if (player.location.s != undefined){
-        player.location = locations[player.location.s];
+        player.setLocation(locations[player.location.s]);
         print('You went South!');
         print('</br>'+player.location.description);
       }else{
@@ -233,7 +234,7 @@ function analyzeInput(switchvar){
       break;
     case 'E': case 'EAST':
       if (player.location.e != undefined){
-        player.location = locations[player.location.e];
+        player.setLocation(locations[player.location.e]);
         print('You went East!');
         print('</br>'+player.location.description);
       }else{
@@ -242,7 +243,7 @@ function analyzeInput(switchvar){
       break;
     case 'W': case 'WEST':
       if (player.location.w != undefined){
-        player.location = locations[player.location.w];
+        player.setLocation(locations[player.location.w]);
         print('You went West!');
         print('</br>'+player.location.description);
       }else{
@@ -261,7 +262,7 @@ function analyzeInput(switchvar){
         $.each(player.location.items, function(index,item){
           $.each(item.keywords, function(i,keyword){
             if (keyword.toUpperCase() == words[wordind]){
-              player.inventory.push(player.location.items[index]);
+              player.addItem(player.location.items[index]);
               player.location.items.splice(index, 1);
               print('Got the '+ item.title);
               gotItem = true; 
@@ -318,147 +319,195 @@ function createInputArea(){
   $('.inputWrap.active input').focus();
 }
 
-
-var debug = false;
-
-var items = {
-     paper :{
-       title: 'Paper',
-       keywords : ['paper'],
-       description : "A peice of paper.  There's nothing written on it."
-     },
-     key_lion :{
-       title : 'Key',
-       keywords : ['key'],
-       atitle : 'Ornate Key (Lion)',
-       description: "It's a key with a lion's head on it."
-     },
-     key_tiger :{
-       title : 'Key',
-       keywords : ['key'],
-       atitle : 'Ornate Key (Tiger)',
-       description: "It's a key with a tiger's skull on it."
-     },     
-     watch :{
-       title: 'Watch',
-       keywords : ['watch']
-     },
-     candle :{
-       title: 'Candle',
-       keywords : ['candle']
-     },
-     shoe :{
-       title: 'Shoe',
-       keywords : ['shoe']
-     }
-}
-
-var objects = {
-  door : {
-    title : 'Door',
-    keywords : ["door"],
-    keys : [items.key]
-  },
-  mirror : {
-    title : 'Mirror',
-    keywords : ["mirror"],
-  }
-}
-var actions = {
-  cavein : {
-    description : "The floor caved in! You can't go back!"
-  }
-}
-
-var locations = {
-    lobby : {
-      title : 'Lobby',
-      description: 'You are in a big lobby. There are ways to all four cardinal directions... On the ground is a paper and a key.',
-      items : [items.paper, items.key_lion],
-      objects : {
-        door : {
-          actions : {
-            look : {
-                description : 'There is a door.'},
-            use : {
-              key_lion : {
-                description: "This key doesn't work in this door!"},
-              key_tiger : {
-                description : "Unlocked the door!",
-                uses : 1,
-                x_description : "The key broke!"}
-            }
-          },
-          locked : 1   
-        },
-        mirror: {
-          actions : {
-            look : {
-              description : 'You see your reflection.'}
-          }
-        }
-      },
-      n: 'forest',
-      e: 'alleyway',
-      s: 'alleyway_2',
-      w: 'dead_end'
-    },
-    forest : {
-      title : 'Forest',
-      description: "A thick forest with a big tree in front, if you don't want get lost, better turn around and head back south...",
-      n: 'lost',
-      s: 'lobby'
-    },
-    alleyway : {
-      title: 'Alleyway',
-      description: "It's an alleyway with 2 trashcans...  Ways leading west and south.",
-      items : [items.watch],
-      w: 'lobby',
-      s: 'river'
-    },
-    river: {
-      title : 'River',
-      description: "It's a riverside with paths heading north and west...",
-      n: 'alleyway',
-      w: 'alleyway_2'
-    },
-    alleyway_2 : {
-      title: 'Alleyway',
-      description: "It's an alleyway with trails on north and east direction...",
-      n: 'lobby',
-      e: 'river'
-    },
-    dead_end : {
-      title: 'Dead end',
-      description: 'Just a deadlock, the only way leads back east...',
-      e: 'lobby'
-      },
-    lost : {
-      title : 'Lost',
-      description: "Nothing but trees...",
-      s: 'forest',
-      e: 'lost',
-      n: 'lost',
-      w: 'lost'
-    }
-}
-
-
-var player = {
-  name : 'Default',
-  location : locations.lobby,
-  inventory : []
-}
-
-
 function introduction(){
   print('Welcome stranger...!</br>');
   print("  [i] - (Enter '?', 'HELP' or 'H' to display help/information...!)", 'pre');
   print('','br');
 }
 
+game = function(){
+  this.engine = {
+    addCommand : function(name, data){
+      commands[name] = data;
+    },
+    addKeyword : function(name, command){
+      keywords[name] = command;
+    },
+    commands : {
+      move : function (){
+
+      }
+    },
+    keywords : [
+    ] 
+  }
+  this.player = {
+    name : 'Default',
+    location : '',
+    inventory : [],
+    move : function (crd_dir){
+      this.engine.commands.move(crd_dir);
+    },
+    setLocation : function(loc){
+      this.location = loc;
+    },
+    setVar : function(variable, val){
+      this[variable] = val;
+    },
+    addItem : function(item){
+      this.inventory.push(item);
+    }
+  }
+  this.addItem = function(name, data){
+    this.items[name] = data;
+  }
+
+  this.addLocation = function(name, data){
+    this.locations[name] = data;
+  }
+  
+  this.addObject = function(name, data){
+    this.objects[name] = data;
+  }
+
+  this.addAction = function(name, data){
+    this.actions[name] = data; 
+  }
+
+  this.setDebugMode = function(mode){
+    this.debug = mode;
+  }
+
+  this.debug = false;
+  this.items = {}
+  this.objects ={}
+  this.locations = {}
+  this.actions = {}
+
+}
+locked_room = new game();
+player = locked_room.player;
+items = locked_room.items;
+locations = locked_room.locations;
+locked_room.addItem('paper',{
+  title: 'Paper',
+  keywords : ['paper'],
+  description : "A piece of paper.  There's nothing written on it."
+});
+locked_room.addItem('key_lion',{
+ title : 'Key',
+ keywords : ['key'],
+ atitle : 'Ornate Key (Lion)',
+ description: "It's a key with a lion's head on it."
+});
+locked_room.addItem('key_tiger',{
+ title : 'Key',
+ keywords : ['key'],
+ atitle : 'Ornate Key (Tiger)',
+ description: "It's a key with a tiger's skull on it."
+});     
+locked_room.addItem('watch',{
+ title: 'Watch',
+ keywords : ['watch']
+});
+locked_room.addItem('candle',{
+ title: 'Candle',
+ keywords : ['candle']
+});
+locked_room.addItem('shoe',{
+ title: 'Shoe',
+ keywords : ['shoe']
+});
+locked_room.addObject('door',{
+  title : 'Door',
+  keywords : ["door"],
+  keys : [items.key]
+});
+locked_room.addObject('mirror',{
+  title : 'Mirror',
+  keywords : ["mirror"],
+});
+locked_room.addAction('cavein', {
+  description : "The floor caved in! You can't go back!"
+});
+locked_room.addLocation('lobby', {
+  title : 'Lobby',
+  description: 'You are in a big lobby. There are ways to all four cardinal directions... On the ground is a paper and a key.',
+  items : [items.paper, items.key_lion],
+  objects : {
+    door : {
+      actions : {
+        look : {
+            description : 'There is a door.'},
+        use : {
+          key_lion : {
+            description: "This key doesn't work in this door!"},
+          key_tiger : {
+            description : "Unlocked the door!",
+            uses : 1,
+            x_description : "The key broke!"}
+        }
+      },
+      locked : 1   
+    },
+    mirror: {
+      actions : {
+        look : {
+          description : 'You see your reflection.'}
+      }
+    }
+  },
+  n: 'forest',
+  e: 'alleyway',
+  s: 'alleyway_2',
+  w: 'dead_end'
+});
+locked_room.addLocation('forest', {
+  title : 'Forest',
+  description: "A thick forest with a big tree in front, if you don't want get lost, better turn around and head back south...",
+  n: 'lost',
+  s: 'lobby'
+});
+locked_room.addLocation('alleyway', {
+  title: 'Alleyway',
+  description: "It's an alleyway with 2 trashcans...  Ways leading west and south.",
+  items : [items.watch],
+  w: 'lobby',
+  s: 'river'
+});
+locked_room.addLocation('river', {
+  title : 'River',
+  description: "It's a riverside with paths heading north and west...",
+  n: 'alleyway',
+  w: 'alleyway_2'
+});
+locked_room.addLocation('alleyway_2', {
+  title: 'Alleyway',
+  description: "It's an alleyway with trails on north and east direction...",
+  n: 'lobby',
+  e: 'river'
+});
+locked_room.addLocation('dead_end', {
+  title: 'Dead end',
+  description: 'Just a deadlock, the only way leads back east...',
+  e: 'lobby'
+});
+locked_room.addLocation('lost', {
+  title : 'Lost',
+  description: "Nothing but trees...",
+  s: 'forest',
+  e: 'lost',
+  n: 'lost',
+  w: 'lost'
+});
+
+
+
+player.setVar('name', 'Steve');
+player.setVar('location', locations.lobby);
 
 $(document).ready(function(){
+  console.log(locked_room);
   introduction();
   requestInput();
   $(window).click(function(){
